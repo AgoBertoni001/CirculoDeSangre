f5f4f1d3d9c06078dc3c18d4284d834f68be9a71
Agostina <Agostina@DESKTOP-F1AHD1I>
2022-04-01 19:24:25 -0300

    Agregar archivos de proyecto.

---
+++ CirculoDeSangre/Donacion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CirculoDeSangre
+{
+    public class Donacion
+    {
+        public int Dia { get;  }
+        public int Mes { get; }
+        public int Año { get; }     
+
+    }
+}
