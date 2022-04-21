00f6008f05156e2b6fff0afe1afae6947d070a63
Agostina Bertoni <abertoni@facultad.sanfrancisco.utn.edu.ar>
2022-04-21 17:28:49 -0300

    actualizacion

--- CirculoDeSangre/Asociado.cs
+++ CirculoDeSangre/Asociado.cs
@@ -6,10 +6,11 @@ using System.Threading.Tasks;
 
 namespace CirculoDeSangre
 {
-    public class Asociado
+    public class Asociado : IEquatable<Asociado>
     {
+               
         //metodo de atributos
-        public long DNI { get; set; }
+        public long DNI { get; set; } 
         public string Nombre { get; set; }
         public string Apellido { get; set; }
         public string Nacimiento { get; set; }
@@ -25,6 +26,7 @@ namespace CirculoDeSangre
         //metodo de constructor parametrizado
         public Asociado (long DNI, string Nombre, string Apellido, string Nacimiento, string Domicilio, long AlturaDomicilio, string Localidad, long Telefono, string Email, bool Enfermedad, bool Medicamentos)
         {
+            
             this.DNI = DNI;
             this.Nombre = Nombre;
             this.Apellido = Apellido;
@@ -36,8 +38,9 @@ namespace CirculoDeSangre
             this.Email = Email;
             this.Enfermedad = Enfermedad;
             this.Medicamentos = Medicamentos;
-        }
-
+       
+        } 
+        
         public void presentarme()
         {
             Console.WriteLine("Vista rápida de datos ingresados: ");
@@ -50,57 +53,82 @@ namespace CirculoDeSangre
         }
         */
 
+        public override string ToString()
+        {
+            return "DNI: " + DNI + " Nombre y Apellido: " + Nombre + Apellido+ " Domicilio " + Domicilio + " Altura " + AlturaDomicilio + " Localidad: " + Localidad + 
+                " \nTelefono: " + Telefono + " Enfermedades y Medicamentos: " + Enfermedad + Medicamentos;
+        }
+
+        public override bool Equals(object obj)
+        {
+            /*if (obj == null) return false;
+            Asociado objAsAsociado = obj as Asociado;
+            if (objAsAsociado == null) return false;
+            else return Equals(objAsAsociado);*/
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            //return DNI.GetHashCode() ^ Nombre.GetHashCode() ^ Apellido.GetHashCode();
+            return base.GetHashCode();
+        }
+
+        public bool Equals(Asociado other)
+        {
+            if (other == null) return false;
+            return (this.DNI.Equals(other.DNI));
+        }
+
+/*
         public static void AltaAsociado()
         {
+            
             Console.Write("DNI (sin espacios ni puntos): ");
             long dni = long.Parse(Console.ReadLine());
-
+            
             Console.Write("Nombre: ");
             string nombre = Console.ReadLine();
-
+           
             Console.Write("Apellido: ");
             string apellido = Console.ReadLine();
-
+            
             Console.Write("Fecha de nacimiento (dia mes año): ");
             string nacimiento = Console.ReadLine();
-
+            
             Console.Write("Domicilio (solo nombre, no altura): ");
             string domicilio = Console.ReadLine();
-
+            
             Console.Write("Altura del Domicilio: ");
             long alturaDomicilio = long.Parse(Console.ReadLine());
-
+            
             Console.Write("Localidad: ");
             string localidad = Console.ReadLine();
-
+            
             Console.Write("Telefono: ");
             long telefono = long.Parse(Console.ReadLine());
-
+            
             Console.Write("Email: ");
             string email = Console.ReadLine();
-
+            
             Console.Write("Enferemdad (1-para Si, 2-para No): ");
             int enfermedad = int.Parse(Console.ReadLine());
-
+                        
             Console.Write("Medicamentos (1-para Si, 2-para No): ");
             int medicamentos = int.Parse(Console.ReadLine());
-
+           
+            
+            //GUARDAR TODO EN UNA LISTA
         }
 
 
-        public void MostrarAlta()
+        public static void MostrarAlta()
         {
-            Console.WriteLine("Vista rápida de datos ingresados: ");
-            Console.WriteLine("Mi DNI es: {0}", DNI);
-            Console.WriteLine("Mi nombre y apellido es " + Nombre + Apellido);
-            Console.WriteLine("Domicilio " + Domicilio + "altura " + AlturaDomicilio);
-            Console.WriteLine("Localidad {0}", Localidad);
-            Console.WriteLine("Telefono: " + Telefono + "Email: " + Email);
-            Console.WriteLine("Enfermedades y medicamentos {0}, {1} respectivamente", Enfermedad, Medicamentos);
+            //mostrar LISTA
+            Console.WriteLine("Hola :)");
+
         }
-
-        
-
+*/
 
     }
 }
--- CirculoDeSangre/Program.cs
+++ CirculoDeSangre/Program.cs
@@ -6,65 +6,62 @@ using System.Threading.Tasks;
 
 //agoBertoni
 
-namespace CirculoDeSangre
-{
-    internal class Program
-    {
+namespace CirculoDeSangre{
+    internal class Program{
 
-        public Asociado asociado = new Asociado(); 
-
-
-        static void Main(string[] args)
-        {
+        Asociado asociadoNuevo = new Asociado(); //creo objeto
+        
+        static void Main(string[] args){
             Menu();
             Console.ReadLine();
         }
 
-
-
-        public static void Menu()
-        {
+        public static void Menu(){
             
-            Console.WriteLine("MENÚ:    \n");
-            Console.WriteLine("Elija la operacion a realizar:\n");
-            Console.WriteLine("1-Alta (oprima tecla 1): \n2-No deseo realizar operaciones (oprima 0): \n");
-            Console.Write("Ingrese su opción: ");
-            int opcion = int.Parse(Console.ReadLine());
-            Console.WriteLine("Su opción fue: {0}", opcion);
+            int opcion = 1;
+            
+            do{
+                Console.WriteLine("MENÚ:    \n");
+                Console.WriteLine("Elija la operacion a realizar:\n");
+                Console.WriteLine("1-Alta (oprima tecla 1): " + "\n2-No deseo realizar operaciones (oprima 0): \n");
+                Console.Write("Ingrese su opción: ");
+                
+                opcion = int.Parse(Console.ReadLine());
+                Console.WriteLine("Su opción fue: {0}", opcion);
+                Opciones(opcion);
 
-            Opciones(opcion);
             
-            
+            }while (opcion!=0);
         }
 
-
-
-
-        public static void Opciones(int opcion)
-        {
-            switch (opcion)
-            {
+        public static void Opciones(int opcion) {
+            
+            switch (opcion) {
+                
                 case 0:
                     Console.WriteLine("Fin del programa."); //No se desea realizar operaciones.
+                    
                     break;
                 
                 case 1:
-                   
-                    Asociado.AltaAsociado();  //llamar a la funcion Alta
-                    
+
+                    List<Asociado> asociados = new List<Asociado>(); //crea lista de tipo Asociado
+                    asociados.Add(new Asociado() { DNI = 43368370, Nombre = "Agostina", Apellido = "Bertoni" });
+                    asociados.Add(new Asociado() { DNI = 39613761, Nombre = "Florencia", Apellido = "Bertoni" });
+                    asociados.Add(new Asociado() { DNI = 41658998, Nombre = "Lucas", Apellido = "Guzman" });
+
+                    //Asociado.AltaAsociado();
+
                     Console.WriteLine("Si desea ver los datos que ingresó presione 1, en caso que no lo desee 0: ");
                     int imprimaDatosIngresados=int.Parse(Console.ReadLine());
                     
                     if (imprimaDatosIngresados == 1) {
-                        Asociado.MostrarAlta();
-                    }
-                    else if (imprimaDatosIngresados==0)
-                    {
+                        //Asociado.MostrarAlta();
+                    }else if (imprimaDatosIngresados==0){
                         Console.WriteLine("No desea ver los resultados");
-
                     }
 
-                    break;
+                   break;
             }
         } 
 
